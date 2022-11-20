80c7c4c8331709aafbd922ee068bd68aba97a58b
Nestor Torres <alu0101021768@ull.edu.es>
2022-11-20 00:56:15 +0000

    refactor: updated gilded rose class to let items update by themselves

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,62 +1,10 @@
-import { Item } from "./item";
-
-const ITEM_NAME_AGED_BRIE = 'Aged Brie';
-const ITEM_NAME_BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
-const ITEM_NAME_SULFURAS = 'Sulfuras, Hand of Ragnaros';
-const ITEM_MIN_QUALITY = 0;
-const ITEM_MAX_QUALITY = 50;
-
-function decreaseSellIn(item: Item) {
-  item.sellIn -= 1
-}
-
-function updateAgedBrieQuality(item: Item) {
-  if (item.quality < ITEM_MAX_QUALITY) {
-    item.quality += 1
-  }
-}
-function updateBackstagePassesQuality(item: Item) {
-  if (item.quality < ITEM_MAX_QUALITY) {
-    item.quality = item.quality + 1
-  }
-}
-function updateDefaultItemQuality(item: Item) {
-  if (item.quality > ITEM_MIN_QUALITY) {
-    item.quality = item.quality - 1
-  }
-}
+import { Item } from "./items/Item";
 
 export class GildedRose {
 
-  items: Array<Item>;
-
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
-  }
+  constructor(public items = [] as Array<Item>) {}
 
   updateQuality() {
-    for (const item of this.items) {
-      switch (item.name) {
-        case ITEM_NAME_AGED_BRIE:
-          decreaseSellIn(item)
-          updateAgedBrieQuality(item)
-          break;
-
-        case ITEM_NAME_BACKSTAGE_PASSES:
-          decreaseSellIn(item)
-          updateBackstagePassesQuality(item)
-          break;
-
-        case ITEM_NAME_SULFURAS:
-          break;
-
-        default:
-          decreaseSellIn(item)
-          updateDefaultItemQuality(item)
-          break;
-      }
-    }
-
-    return this.items;
+    this.items.forEach((item) => item.update())
   }
 }
